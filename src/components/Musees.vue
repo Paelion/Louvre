04a78cda4a3cd3e9e767551cadf9e18e2b3074b5
Paelion <aelion.pauline1@gmail.com>
2020-05-18 13:22:16 +0200

    add responsive musee

--- src/components/Musees.vue
+++ src/components/Musees.vue
@@ -2,13 +2,13 @@
     <div class="musees">
         <h3>{{ $t('musees.titre') }}</h3>
         <div class="musees--wraper">
-            <router-link :to="`/${$i18n.locale}/lens`" class="home--link">
+            <router-link :to="`/${$i18n.locale}/lens`" class="musees--link">
                 <div class="lens--wrapper">
                     <img src="../../public/img/louvreLens2.jpg" class="lens">
                     <h4 class="lens--title">{{ $t('musees.lens') }}</h4>
                 </div>
             </router-link>
-            <router-link :to="`/${$i18n.locale}/ad`" class="home--link">
+            <router-link :to="`/${$i18n.locale}/ad`" class="musees--link">
                 <div class="ad--wrapper">
                     <img src="../../public/img/louvreAD2.jpg" class="ad">
                     <h4 class="ad--title">{{ $t('musees.ad') }}</h4>
